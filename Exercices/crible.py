07a438eb7c496a6119b67eb482d168043174465f
Yannick Copin <y.copin@ipnl.in2p3.fr>
2012-08-31 19:21:58 +0200

    Retrait des symboles unicodes pour la compilation (pdf)LaTeX. Ajout de la gestion simplifiée des arguments de la ligne de commande via sys. Ajout de liens pour print/raw_input.
    Conflicts:
    
    	Cours/cours.txt
    	Exercices/crible.py
    	Exercices/exo.txt

--- Exercices/crible.py
+++ Exercices/crible.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Time-stamp: <2012-09-29 00:12 ycopin@lyopc469>
+# Time-stamp: <2013-10-23 23:43 ycopin@lyonovae03.in2p3.fr>
 
 """
 Crible d'Ératosthène.
@@ -8,7 +8,7 @@ Crible d'Ératosthène.
 Source: http://fr.wikibooks.org/wiki/Exemples_de_scripts_Python#Impl.C3.A9mentation_du_crible_d.27.C3.89ratosth.C3.A8ne
 """
 
-# Gestion simplifiée d'un argument sur la ligne de commande
+# Gestion simplifiée d'un argument sur la ligne de commande (TD4)
 # start-sys
 import sys
 
@@ -24,14 +24,14 @@ else:                        # Pas d'argument sur la ligne de commande
 # Liste des entiers *potentiellement* premiers. Les nb non-premiers
 # seront étiquetés par 0.
 l = range(n+1)                          # <0,...,n>
-l[1] = 0                                # 1 n'est pas premiers
+l[1] = 0                                # 1 n'est pas premier
 
 i = 2                                   # Entier à tester
 while i**2 <= n:                        # Inutile de tester jusqu'à n
     if l[i]:                            # Si i n'est pas étiqueté...
-        l[2*i::i] = [None]*len(l[2*i::i]) # ...étiqueter tous les multiples de i
+        l[2*i::i] = [0]*len(l[2*i::i])  # ...étiqueter tous les multiples de i
     i += 1                              # Passer à l'entier à tester suivant
 
-# Afficher la liste des entiers premiers (non-étiquetés)
+# Afficher la liste des entiers premiers (c-à-d non-étiquetés)
 print "Liste des entiers premiers <=", n
 print [ i for i in l if i ]
