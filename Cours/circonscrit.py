75b0ca86e8f454b771d0748ff4a0d5a1516deb2f
Yannick Copin <yannick.copin@laposte.net>
2014-01-12 23:51:00 +0100

    Débug de 'circonscrit.py', ajout des solutions aux exercices numériques.

--- Cours/circonscrit.py
+++ Cours/circonscrit.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__author__ = "Yannick Copin <y.copin@ipnl.in2p3.fr>"
-__version__ = "Time-stamp: <2014-01-08 22:01:24 ycopin>"
-
 """
 Calcul du cercle circonscrit à 3 points du plan.
 """
+
+__author__ = "Yannick Copin <y.copin@ipnl.in2p3.fr>"
+__version__ = "Time-stamp: <2014-01-12 22:19 ycopin@lyonovae03.in2p3.fr>"
 
 from math import sqrt, hypot
 
@@ -25,10 +25,10 @@ class Point(object): # *object* est la classe dont dérivent toutes les autres
             self.x = float(x)
             self.y = float(y)
         except ValueError:
-            raise ValueError("Incompatible input coordinates")
+            raise ValueError("Coordonnées d'entrée invalides")
 
     def __str__(self):
-        """Surcharge de l'opérateur `str`: l'affichage *informel* de
+        """Surcharge de la fonction `str()`: l'affichage *informel* de
         l'objet dans l'interpréteur, p.ex. `print self` sera résolu
         comme `self.__str__()`
 
@@ -37,16 +37,17 @@ class Point(object): # *object* est la classe dont dérivent toutes les autres
 
         return "Point (x=%f, y=%f)" % (self.x, self.y)
 
-    def isOrigin(self):
-        """Méthode booléenne, vrai si le point est à l'origine."""
+    def __nonzero__(self):
+        """Surcharge de la fonction `bool()`: `bool(self)` retourne
+        vrai si le point n'est pas à l'origine."""
 
-        return ((self.x == 0) and (self.y == 0))
+        return ((self.x != 0) and (self.y != 0))
 
     def distance(self, other):
         """Méthode de calcul de la distance du point (`self`) à un
         autre (`other`)."""
 
-        return hypot(self.x - other.x, self.y - other.y) # sqrt(dx² + dy²)
+        return hypot(self.x - other.x, self.y - other.y) # sqrt(dx**2 + dy**2)
 # end-classPoint
 
 # Définition du point origine O
@@ -55,86 +56,81 @@ O = Point(0,0)
 # Héritage de classe ==============================
 
 # start-classVector
-class Vector(Point):
-    """Un `Vector` hérite de `Point` avec des méthodes additionnelles
+class Vecteur(Point):
+    """Un `Vecteur` hérite de `Point` avec des méthodes additionnelles
     (p.ex. l'addition)."""
 
-    def __init__(self, A, B=O):
-        """Définit le vecteur :math:`\vec{AB}` si B!=O, ou
-        :math:`\vec{OA}` sinon."""
+    def __init__(self, A, B):
+        """Définit le vecteur :math:`\vec{AB}` à partir des 2 points
+        `A` et `B`."""
 
         # Initialisation de la classe parente
-        if B.isOrigin():                # B = O
-            Point.__init__(self, A.x, A.y)
-        else:                           # B != O
-            Point.__init__(self, B.x-A.x, B.y-A.y)
-            
+        Point.__init__(self, B.x-A.x, B.y-A.y)
+
         # Attribut propre à la classe dérivée
         self.norm2 = self.x**2 + self.y**2 # Norme du vecteur au carré
 
     def __str__(self):
         """Surcharge de la fonction `str()`: ainsi, `print self` sera
-        résolu comme `Vector.__str__(self)` (et non pas comme
+        résolu comme `Vecteur.__str__(self)` (et non pas comme
         `Point.__str__(self)`)
         """
 
-        return "Vector (x=%f, y=%f)" % (self.x, self.y)
+        return "Vecteur (x=%f, y=%f)" % (self.x, self.y)
 
     def __add__(self, other):
         """Surcharge de l'opérateur `+`: l'instruction `self + other`
         sera résolue comme `self.__add__(other)`.
 
-        On construit une nouvelle instance de `Vector` à partir des
+        On construit une nouvelle instance de `Vecteur` à partir des
         coordonnées propres à l'objet `self`, et à l'autre opérande
         `other`."""
 
-        return Vector((self.x + other.x, self.y + other.y))
-
-    isNull = Point.isOrigin       # Vector.isNull() = Point.isOrigin()
+        return Vecteur(O, Point(self.x + other.x, self.y + other.y))
 
     def __abs__(self):
         """Surcharge de la fonction `abs()`"""
 
-        # Il vaudrait mieux sqrt(self.norm2), mais c'est pour
+        # Il vaudrait utiliser sqrt(self.norm2), mais c'est pour
         # l'exemple d'utilisation d'une méthode héritée...
         return Point.distance(self, O)
 # end-classVector
 
 
-def circumCircle(M,N,P):
+def cercleCirconscrit(M,N,P):
     """Calcule le centre et le rayon du cercle circonscrit aux
     points M,N,P.
 
-    Retourne: centre (Point),rayon (float)"""
+    Retourne: (centre [Point],rayon [float])"""
 
-    MN = Vector(M,N)
-    NP = Vector(N,P)
-    PM = Vector(P,M)
-    
-    # Diamètre
+    MN = Vecteur(M,N)
+    NP = Vecteur(N,P)
+    PM = Vecteur(P,M)
+
+    # Rayon
     m = abs(NP)                         # |NP|
     n = abs(PM)                         # |PM|
     p = abs(MN)                         # |MN|
 
     d = (m+n+p)*(-m+n+p)*(m-n+p)*(m+n-p)
-    if d>0: 
-        diam = 2*m*n*p / sqrt(d)
+    if d>0:
+        rad = m*n*p / sqrt(d)
     else:
-        raise ValueError("Undefined circumscribed circle diameter.")
+        raise ValueError("Rayon du cercle circonscrit indéfini.")
 
     # Centre
     d = -2*( M.x*NP.y + N.x*PM.y + P.x*MN.y )
     if d==0:
-        raise ValueError("Undefined circumscribed circle center.")
-    
-    om2 = Vector(O,M).norm2             # |OM|²
-    on2 = Vector(O,N).norm2             # |ON|²
-    op2 = Vector(O,P).norm2             # |OP|²
+        raise ValueError("Centre du cercle circonscrit indéfini.")
+
+    om2 = Vecteur(O,M).norm2             # |OM|**2
+    on2 = Vecteur(O,N).norm2             # |ON|**2
+    op2 = Vecteur(O,P).norm2             # |OP|**2
 
     x0 = -( om2*NP.y + on2*PM.y + op2*MN.y ) / d
     y0 =  ( om2*NP.x + on2*PM.x + op2*MN.x ) / d
-
-    return Point(x0,y0),diam/2.         # Centre,R
+    
+    return (Point(x0,y0), rad)           # (Centre,R)
 
 
 if __name__=='__main__':
@@ -142,16 +138,16 @@ if __name__=='__main__':
     # start-optparse
     from optparse import OptionParser
 
-    description = "Calcul du cercle circonscrit à 3 points du plan."
+    usage = "%prog [-p/--plot] [-i/--input coordfile | x1,y1 x2,y2 x3,y3]"
+    description = u"Calcul du cercle circonscrit à 3 points du plan."
 
-    parser = OptionParser(usage="%prog [-p/--plot] "
-                          "[-i/--input coordfile | x1,y1 x2,y2 x3,y3]",
-                          version=__version__, description=description)
+    parser = OptionParser(usage=usage, description=description,
+                          version=__version__)
     parser.add_option("-i", "--input",
-                      help="Input coordinate file (one 'x,y' per line)")
+                      help=u"Fichier de coordonnées (un 'x,y' par ligne)")
     parser.add_option("-p", "--plot",
                       action="store_true", default=False,
-                      help="Plot circumscribed circle")
+                      help="Trace le cercle circonscrit")
 
     opts,args = parser.parse_args()
     # end-optparse
@@ -160,40 +156,42 @@ if __name__=='__main__':
         try:
             args = file(opts.input).readlines()
         except IOError:
-            parser.error("Cannot read coordinates from %s" % opts.input)
-            
+            parser.error("Impossible de lire le fichier '%s'" % opts.input)
+
     if len(args) != 3:                  # Vérifie le nb de points
-        parser.error("Specify 3 input points by their 'x,y' coordinates")
+        parser.error("Spécifier 3 points par leurs coordonnées 'x,y'")
 
     points = [None]*3                   # Génère la liste de 3 points
     for i,arg in enumerate(args):
         try:                            # Déchiffrage de l'argument 'x,y'
             x,y = ( float(t) for t in arg.split(',') )
         except ValueError:
-            parser.error("Cannot parse x,y coordinates '%s'" % arg)
+            parser.error("Impossible de déchiffrer les coordonnées '%s'" % arg)
 
         points[i] = Point(x,y)          # Création du point
         print "#%d: %s" % (i+1,str(points[i]))
 
     # Calcul du cercle cisconscrit
     try:
-        center,radius = circumCircle(*points) # Délistage
-        print "Circumscribed circle: %s, radius=%f" % (str(center),radius)
+        centre,rayon = cercleCirconscrit(*points) # Délistage
+        print "Cercle circonscrit: %s, rayon=%f" % (str(centre),rayon)
     except ValueError:
-        raise ValueError("Undefined circumscribed circle")
+        raise ValueError("Circle circonscrit indéfini")
 
-    if opts.plot:
-        # Figure
+    if opts.plot:                       # Figure
         import matplotlib.pyplot as P
-        
+
         fig = P.figure()
-        ax = fig.add_subplot(1,1,1, aspect='equal', adjustable='datalim')
-        # Point
+        ax = fig.add_subplot(1,1,1, aspect='equal')
+        # Points
         ax.plot([ p.x for p in points], [ p.y for p in points ], 'ko')
+        for i,p in enumerate(points):
+            ax.annotate("#%d" % (i+1), (p.x,p.y),
+                        xytext=(5,5), textcoords='offset points')
         # Cercle circonscrit
-        c = P.matplotlib.patches.Circle((center.x,center.y), radius=radius,
+        c = P.matplotlib.patches.Circle((centre.x,centre.y), radius=rayon,
                                         fc='none')
         ax.add_patch(c)
-        ax.plot(center.x,center.y,'r+')
-        
+        ax.plot(centre.x,centre.y,'r+')
+
         P.show()
