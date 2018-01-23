91192cace07eca8655f1a5a050f893dad6e59710
Yannick Copin <y.copin@ipnl.in2p3.fr>
2018-01-23 17:19:10 +0100

    Pas d'utilisation de seaborn dans exemple figure.py.

--- Cours/figure.py
+++ Cours/figure.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Time-stamp: <2017-12-06 18:16:15 ycopin>
+# Time-stamp: <2018-01-23 17:15:04 ycopin>
 
 """
 Exemple un peu plus complexe de figure, incluant 2 axes, légendes, axes, etc.
@@ -8,10 +8,6 @@ Exemple un peu plus complexe de figure, incluant 2 axes, légendes, axes, etc.
 
 import numpy as N
 import matplotlib.pyplot as P
-try:
-    import seaborn              # Amélioration de la charte graphique
-except ImportError:
-    print u"Seaborn n'est pas accessible, charte graphique par défaut."
 
 x = N.linspace(-N.pi, 3*N.pi, 2*360)
 
