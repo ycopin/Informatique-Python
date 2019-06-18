b59918d1e913cf2b7807770494e79dd8e95aa786
Yannick Copin <y.copin@ipnl.in2p3.fr>
2019-06-18 12:20:51 +0200

    pm.py: nouvelle solution.

--- Exercices/pm.py
+++ Exercices/pm.py
@@ -7,27 +7,32 @@ nmin, nmax = 1, 100
 nsol = random.randint(nmin, nmax)
 
 print("Vous devez deviner un nombre entre {} et {}.".format(nmin, nmax))
-ncoups = 0
-try:
-    while True:
+ncoups = 0                      # Compteur de coups
+
+while True:                     # Boucle infinie: sortie explicite par break
+    try:
         proposition = input("Votre proposition: ")
-        try:
-            ntest = int(proposition)
-            if not nmin <= ntest <= nmax:
-                raise ValueError("Proposition invalide")
-        except ValueError:
-            print("Votre proposition {!r} n'est pas un entier compris entre {} et {}."
-                  .format(proposition, nmin, nmax))
-            continue
-        ncoups += 1
-        if nsol > ntest:
-            print("C'est plus.")
-        elif nsol < ntest:
-            print("C'est moins.")
-        else:
-            print("Vous avez trouvé en {} coup{}!".format(
-                ncoups, 's' if ncoups > 1 else ''))
-            break  # Interrompt la boucle while
-except (KeyboardInterrupt, EOFError):  # Interception de Ctrl-C et Ctrl-D
-    print("\nVous abandonnez après seulement {} coup{}!"
-          .format(ncoups, 's' if ncoups > 1 else ''))
+        ntest = int(proposition)       # Exception ValueError potentielle
+        if not nmin <= ntest <= nmax:
+            raise ValueError()
+
+    except ValueError:
+        print("Votre proposition {!r} n'est pas un entier compris entre {} et {}."
+              .format(proposition, nmin, nmax))
+        continue                # Nouvel essai
+
+    except (KeyboardInterrupt, EOFError):  # Interception de Ctrl-C et Ctrl-D
+        print("\nVous abandonnez après seulement {} coup{}!"
+              .format(ncoups, 's' if ncoups > 1 else ''))
+        break                   # Interrompt la boucle while
+
+    # Si la proposition est valide, le jeu peut continuer.
+    ncoups += 1
+    if nsol > ntest:
+        print("C'est plus.")
+    elif nsol < ntest:
+        print("C'est moins.")
+    else:
+        print("Vous avez trouvé en {} coup{}!"
+              .format(ncoups, 's' if ncoups > 1 else ''))
+        break                   # Interrompt la boucle while
