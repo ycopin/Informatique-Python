9ce96bfc1f5d65298a24d0a6475d911d1642db08
Yannick Copin <y.copin@ipnl.in2p3.fr>
2020-11-16 21:55:05 +0100

    do-while, pgcd.

--- Exercices/pgcd.py
+++ Exercices/pgcd.py
@@ -1,29 +1,35 @@
 #!/usr/bin/env python3
-# Time-stamp: <2018-07-26 16:56 ycopin@lyonovae03.in2p3.fr>
+# coding: utf-8
+
+# Time-stamp: <2020-11-16 21:52 ycopin@lyonovae03>
 
 """
-Calcul du PGCD de deux entiers.
+Calcul du PGCD de deux entiers 0 < b < a.
 """
 
-# Entiers dont on calcule le PGCD
-a = 306
-b = 756
+# Entiers dont on calcule le PGCD (avec 0 < b < a)
+a = 756
+b = 306
 
-# Test usuels : les nombres sont bien positifs et a > b
-assert (a > 0) and (b > 0)
-if a < b:
-    a, b = b, a                   # Interversion: on suppose a >= b
+# Vérification des conditions d'application de l'algorithme: 0 < b < a
+assert 0 < b < a, "Les conditions d'application ne sont pas vérifiées."
 
 a0, b0 = a, b                     # On garde une copie des valeurs originales
 
 # On boucle jusqu'à ce que le reste soit nul, d'où la boucle while. Il faut
 # être sûr que l'algorithme converge dans tous les cas!
-while b != 0:
-    # On remplace a par b et b par le reste de la division euclidienne
-    # de a par b
-    a, b = b, a % b
+while True:
+    r = a % b
+    if r == 0:                  # Reste de la division euclidienne
+        break                   # en sortie, PGCD = b
+    else:
+        a, b = b, r             # Itération
 
-print('Le PGCD de', a0, 'et', b0, 'vaut', a)  # On affiche le résultat
+# On aurait pu écrire directement:
+# while b != 0:
+#     a, b = b, a % b             # en sortie, PGCD = a!
+
+print('Le PGCD de', a0, 'et', b0, 'vaut', b)  # On affiche le résultat
 # Vérifications
-print(a0 // a, '×', a, '=', (a0 // a * a))  # a//b: division euclidienne
-print(b0 // a, '×', a, '=', (b0 // a * a))
+print(a0 // b, '×', b, '=', (a0 // b * b))  # a//b: division euclidienne
+print(b0 // b, '×', b, '=', (b0 // b * b))
